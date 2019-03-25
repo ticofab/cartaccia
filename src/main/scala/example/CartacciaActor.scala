9086360bf4e3d38e7de02a10f2f87b3abff25baf
Fabio Tiriticco <ticofab@users.noreply.github.com>
2019-03-25 08:24:25 +0100

    comments

--- src/main/scala/example/CartacciaActor.scala
+++ src/main/scala/example/CartacciaActor.scala
@@ -4,19 +4,22 @@ import akka.actor.Actor
 
 class CartacciaActor extends Actor {
 
-  var lastMessage = "niente"
+  // actor's internal state
+  var lastMessage = "niente" // this initial value means "nothing" in Italian
 
   override def receive = {
 
     case "fabio" =>
+      // do nothing so that a TimeoutException will be triggered somewhere else
 
     case s: String =>
       lastMessage = s
       println("I received: " + s)
-      val response = s + " è na cartaccia!"
+      val response = s + " è na cartaccia!" // dialect from the mid-90s in Rome, Italy
       sender ! response
 
     case GiveLastMessage =>
+      // this is the only way to extract the internal state of the actor: ask for it
       sender ! lastMessage
 
   }
